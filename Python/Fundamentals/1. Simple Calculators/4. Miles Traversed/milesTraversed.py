13a6f28ec17f8ede8ca3e731eb49be8021fe69b4
darioGerussi <gerussi@pm.me>
2022-05-17 21:38:32 -0500

    Create milesTraversed.py

---
+++ Python/Fundamentals/1. Simple Calculators/4. Miles Traversed/milesTraversed.py
@@ -0,0 +1,26 @@
+# Displays how many miles a car traversed
+# given a set speed and 3 different durations
+
+# Declare variables
+mph = 70         
+hours1 = 6
+hours2 = 10
+hours3 = 15
+
+# Calculate and display distance
+# for 6hrs at 70mph
+distance1 = mph * hours1
+print('\nThe miles that a car will traverse in ', hours1, 
+      'hrs at ', mph, 'mph is ', distance1, ' miles.', sep='')
+
+# Calculate and display distance
+# for 10hrs at 70mph
+distance2 = mph * hours2
+print('The miles that a car will traverse in ', hours2, 
+      'hrs at ', mph, 'mph is ', distance2, ' miles.', sep='')
+
+# Calculate and display distance
+# for 15hrs at 70mph
+distance3 = mph * hours3
+print('The miles that a car will traverse in ', hours3, 
+      'hrs at ', mph, 'mph is ', distance3, ' miles.\n', sep='')
