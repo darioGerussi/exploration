d1bd5a8c29ea5958e67d47be8a4acfd2d3597611
darioGerussi <gerussi@pm.me>
2022-06-23 22:20:47 -0500

    Create totalPriceCalculator.cpp

---
+++ C++/01. Fundamentals/02. Basic Programs/03. Total Price/totalPriceCalculator.cpp
@@ -0,0 +1,28 @@
+// Calculates total price of purchase
+// including state and county sales tax
+
+#include <iostream>
+
+int main()
+{
+    // Declare variables
+    double itemPrice = 95.00,           // Dollars
+           stateSalesTaxRatio = 0.065,  // Percent
+           countySalesTaxRatio = 0.02,  // Percent
+           stateSalesTaxAmount,
+           countySalesTaxAmount,
+           totalSalesTaxAmount,
+           totalPrice;
+
+    // Calculate state, county, and total
+    // sales taxes
+    stateSalesTaxAmount = itemPrice * stateSalesTaxRatio;
+    countySalesTaxAmount = itemPrice * countySalesTaxRatio;
+    totalSalesTaxAmount = stateSalesTaxAmount + countySalesTaxAmount;
+
+    // Calculate and display total price
+    totalPrice = itemPrice + totalSalesTaxAmount;
+    std::cout << "\nItem Price:    $" << itemPrice;
+    std::cout << "\nSales Tax:     $" << totalSalesTaxAmount;
+    std::cout << "\nTotal Price:   $" << totalPrice << std::endl << std::endl;
+}
